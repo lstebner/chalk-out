422d9574137d462083af60bd24805434855bf32a
lstebner <luke.stebner@gmail.com>
2022-04-24 15:48:17 -0700

    feat: a few tiny features
        - text wrap on project description
        - adding an 'in progress' tab to the swimtabs
        - some overlays to black out certain areas
        - some theming

---
+++ project/src/scenes/gui/InProgressTasks.gd
@@ -0,0 +1,34 @@
+extends Control
+
+signal task_selected(task)
+
+var project = null
+var tasks = []
+
+onready var item_list = $MarginContainer/ItemList
+
+func _ready():
+    item_list.connect("item_selected", self, "_on_item_selected")
+
+func _draw():
+    var selected_items = item_list.get_selected_items()
+    
+    item_list.clear()
+    tasks = []
+    
+    if !project: return
+    
+    for task in project.tasks:
+        if task.current_state != Constants.TASK_STATES.COMPLETED and task.current_state != Constants.TASK_STATES.OPEN:
+            tasks.append(task)
+            item_list.add_item(task.subject)
+        
+    for item in selected_items:
+        item_list.select(item, selected_items.size() == 1)
+
+func set_project(value):
+    project = value
+    update()
+
+func _on_item_selected(index):
+    emit_signal("task_selected", tasks[index])
--- project/src/scenes/main.gd
+++ project/src/scenes/main.gd
@@ -4,60 +4,64 @@ onready var create_menu = $VBoxContainer/MenuBar/MarginContainer/CreateMenuButto
 onready var new_project_dialog = $NewProjectDialog
 onready var projects_list = $VBoxContainer/Main/HBoxContainer/VBoxContainer/ProjectsList
 onready var project_info = $VBoxContainer/Main/HBoxContainer/VBoxContainer/ProjectInfo
-onready var tasks_list = $VBoxContainer/Main/HBoxContainer/TabContainer/Incomplete/TasksList
+onready var tasks_list = $VBoxContainer/Main/HBoxContainer/TabContainer/Backlog/TasksList
 onready var completed_tasks = $VBoxContainer/Main/HBoxContainer/TabContainer/Completed/CompletedTasks
+onready var in_progress_tasks = $"VBoxContainer/Main/HBoxContainer/TabContainer/In Progress/InProgressTasks"
 onready var task_details = $VBoxContainer/Main/HBoxContainer/TaskDetails
 onready var import_file_dialog = $ImportFileDialog
 onready var export_file_dialog = $ExportFileDialog
 
 func _ready():
-	AppCore.connect("project_created", self, "_on_project_created")
-	create_menu.connect("item_selected", self, "_on_create_menu_item_selected")
-	projects_list.connect("project_selected", self, "_on_project_selected")
-	tasks_list.connect("task_selected", self, "_on_task_selected")
-	task_details.connect("task_updated", self, "_on_task_details_updated")
-	project_info.connect("info_updated", self, "_on_project_info_updated")
-	import_file_dialog.connect("file_selected", self, "_on_import_dialog_file_selected")
-	export_file_dialog.connect("file_selected", self, "_on_export_dialog_file_selected")
-	completed_tasks.connect("task_selected", self, "_on_task_selected")
-	
+    AppCore.connect("project_created", self, "_on_project_created")
+    create_menu.connect("item_selected", self, "_on_create_menu_item_selected")
+    projects_list.connect("project_selected", self, "_on_project_selected")
+    tasks_list.connect("task_selected", self, "_on_task_selected")
+    task_details.connect("task_updated", self, "_on_task_details_updated")
+    project_info.connect("info_updated", self, "_on_project_info_updated")
+    import_file_dialog.connect("file_selected", self, "_on_import_dialog_file_selected")
+    export_file_dialog.connect("file_selected", self, "_on_export_dialog_file_selected")
+    completed_tasks.connect("task_selected", self, "_on_task_selected")
+    in_progress_tasks.connect("task_selected", self, "_on_task_selected")
+    
 func _on_create_menu_item_selected(item_name):
-	match item_name:
-		"New Project":
-			new_project_dialog.show()
-		
-		"Export Data":
-			export_file_dialog.show()
-		
-		"Import Data":
-			import_file_dialog.show()
+    match item_name:
+        "New Project":
+            new_project_dialog.show()
+        
+        "Export Data":
+            export_file_dialog.show()
+        
+        "Import Data":
+            import_file_dialog.show()
 
 func _on_project_selected(project_id):
-	var project = AppData.get_project(project_id)
-	tasks_list.set_project_id(project_id)
-	completed_tasks.set_project(project)
-	project_info.set_project(project)
+    var project = AppData.get_project(project_id)
+    tasks_list.set_project_id(project_id)
+    completed_tasks.set_project(project)
+    in_progress_tasks.set_project(project)
+    project_info.set_project(project)
 
 func _on_project_created():
-	new_project_dialog.hide()
+    new_project_dialog.hide()
 
 func _on_task_selected(task):
-	task_details.set_task(task)
+    task_details.set_task(task)
 
 func _on_task_details_updated():
-	AppCore.save()
-	tasks_list.update()
-	completed_tasks.update()
-	
+    AppCore.save()
+    tasks_list.update()
+    completed_tasks.update()
+    in_progress_tasks.update()
+    
 func _on_project_info_updated():
-	AppCore.save()
-	projects_list.update()
+    AppCore.save()
+    projects_list.update()
 
 func _on_import_dialog_file_selected(filename):
-	var filedata = FileHelper.read(filename)
-	AppData.load_state(filedata)
-	get_tree().reload_current_scene()
+    var filedata = FileHelper.read(filename)
+    AppData.load_state(filedata)
+    get_tree().reload_current_scene()
 
 func _on_export_dialog_file_selected(filename):
-	var save_data = AppData.get_save_data()
-	FileHelper.write(filename, save_data)
+    var save_data = AppData.get_save_data()
+    FileHelper.write(filename, save_data)
